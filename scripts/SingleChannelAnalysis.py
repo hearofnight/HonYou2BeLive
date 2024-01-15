7e8845c554d9572e294dd352415129cce8bf8893
hearofnight <63850765+hearofnight@users.noreply.github.com>
2024-01-15 19:30:40 +0800

    Update SingleChannelAnalysis.py

--- scripts/SingleChannelAnalysis.py
+++ scripts/SingleChannelAnalysis.py
@@ -10,12 +10,12 @@ def grab(url):
         response = requests.get(url).text
         if '.m3u8' not in response:
             if windows:
-                print('https://raw.githubusercontent.com/HonYou2BeLive/Channel-Offline/main/offline.ts')
+                print('https://raw.githubusercontent.com/hearofnight/HonYou2BeLive/main/assets/offline.ts')
                 return
             os.system(f'curl "{url}" > temp.txt')
             response = ''.join(open('temp.txt').readlines())
             if '.m3u8' not in response:
-                print('https://raw.githubusercontent.com/HonYou2BeLive/Channel-Offline/main/offline.ts')
+                print('https://raw.githubusercontent.com/hearofnight/HonYou2BeLive/main/assets/offline.ts')
                 return
     end = response.find('.m3u8') + 5
     tuner = 100
