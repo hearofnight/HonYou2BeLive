6d9cc562340968a2adb45b589bc965cc28994ed8
hearofnight <63850765+hearofnight@users.noreply.github.com>
2024-03-29 11:07:38 +0800

    Update singledell.sh

--- singledell.sh
+++ singledell.sh
@@ -4,14 +4,15 @@ export TZ=Asia/Hong_Kong
 # 从URL读取频道列表
 #CHANNEL_URL="https://raw.githubusercontent.com/hearofnight/HonYou2BeLive/main/sources/ChannelNeed.txt"
 #CHANNELS=($(curl -s "$CHANNEL_URL"))
-CHANNELS=("NBCNews" "TvbsHD" "TvbsNews")
+CHANNELS=("TvbsHD")
 #初始化频道信息文件夹和日志文件
 mkdir -p {newchannels,logs}
 touch logs/youtube_live_`date +%Y%m%d`.log
 # 遍历每个频道
 for channel in "${CHANNELS[@]}"; do
     # 调用 xxx.py 脚本并传递频道参数
-    python3 scripts/SingleChannelAnalysis.py "$channel" > "newchannels/$channel.m3u8"
+    #python3 scripts/SingleChannelAnalysis.py "$channel" > "newchannels/$channel.m3u8"
+    python3 scripts/ApiSingleChannelAnalysis.py "$channel" > "newchannels/$channel.m3u8"
     sleep 5
 done
 #保存执行日志到logs/youtube_live_`date +%Y%m%d`.log
